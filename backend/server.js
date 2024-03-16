49832650086469d8bb3d0b796ef8ad04e490b83a
Mickey <mickeymanohar00@gmail.com>
2024-03-16 23:39:28 +0530

    Deployment Ready

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,8 @@
+import path from 'path'
+import { fileURLToPath } from 'url' // Import fileURLToPath function
+
+// Use fileURLToPath to get the directory path
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -17,6 +22,12 @@ app.use(cookieParser())
 app.use('/api/auth', authRouter)
 app.use('/api/messages', messageRouter)
 app.use('/api/user', userRouter)
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 server.listen(port, () => {
   connectToDB()
   console.log(`Server is listening on the port ${port}`)
