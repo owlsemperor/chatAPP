e08aca82fda47458500af72a83940b887a2c9b53
Mickey <mickeymanohar00@gmail.com>
2024-03-17 20:11:02 +0530

    pre production

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.Mongo_URI)
+
     console.log('Connected to MongoDB')
   } catch (error) {
     console.log('Error connecting to database', error.message)
